ced80fc20af44f3edcd3dfe72e2e35f18b14da14
cmaheras <camheras@gmail.com>
2021-06-15 15:39:30 +0200

    [ADD] Parcours de la liste des bien a vendre, et recuperation du prix et de la date.

---
+++ app.py
@@ -0,0 +1,29 @@
+from itertools import groupby
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import re
+
+r = requests.get('http://www.encheres-publiques.com/annuaire/fiche_vente_en_cours.php?idp=144')
+soup = BeautifulSoup(r.text, 'html.parser')
+# print(soup)
+links = soup.findAll('table', style='border:1px solid #fff;')
+# print(links)
+
+formatted_links = []
+
+for link in links:
+    cleanr = re.findall('<a href.*>', str(link))
+    req = requests.get(cleanr[0].split("\"")[1])
+    s = BeautifulSoup(req.text, 'html.parser')
+    # print(s)
+    data = s.findAll('b')
+    price = s.find('b', style="color:#C85E2C;").text
+    for l in data:
+        if l.find(text=re.compile("Visite")):
+            date_visite = str(l).split("place ")[1].split("<")[0]
+    type = s.find('a')
+
+    print(data)
